96ef3e53d1a74abff5511c683573af448caef62a
Ali Al-Saleh <3lialsale7@gmail.com>
2022-01-17 02:48:45 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app-heading mt-5">
+      <h2>OCR App</h2>
+      <p>Optical Character Recognition</p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card mt-4">
+                    <div class="card-header">Uplaod Image or PDF</div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="form-group mt-2">
+                                <label class="mb-3">Image</label>
+                                <input type="file" class="form-control" id="image">
+                            </div>
+                            <div class="form-group mt-2">
+                                <label class="mb-3">Language</label>
+                                <select class="form-control" v-model="language">
+                                    <option value="eng">English</option>
+                                    <option value="ara">Arabic</option>
+                                </select>
+                            </div>
+                            <div class="form-group mt-3 text-center">
+                                <b-button variant="success" @click="convert">Convert</b-button>
+                            </div>
+                        </form>
+                        <div class="text-center mt-4" v-if="loading">
+                            <h2>Loading.....</h2>
+                        </div>
+                        <div class="result text-right" v-if="result">
+                            <label>Result:</label>
+                            <textarea class="form-control text-center mt-3" rows="10" :value="result" disabled></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+      return {
+          result: "",
+          loading: false,
+          image: null,
+          language: null
+      }
+  },
+  methods: {
+      convert() {
+          this.loading = true
+          const axios = require('axios');
+          const fd = new FormData();
+          let image = document.getElementById('image').files[0]
+          fd.append("file", image)
+          fd.append("language", this.language)
+          fd.append("isOverlayRequired", true)
+          axios({
+              method: "post",
+              url: "https://api.ocr.space/parse/image",
+              data: fd,
+              headers: {"Content-Type": "multipart/form-data", "apikey": "d61e1e3a5c88957"},
+          })
+          .then((res) => {
+              console.log(res.data)
+              this.result = res.data.ParsedResults[0].ParsedText
+              this.loading = false
+          })
+          .catch((err) => {
+              console.log(err)
+          })
+      }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #f9f9f9;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  margin: 20px 0;
+}
+.app-heading {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// const axios = require('axios');
+// import axios from 'axios';
+
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
+// Vue.use(axios);
 
 Vue.config.productionTip = false
 
